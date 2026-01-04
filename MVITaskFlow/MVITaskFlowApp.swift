1397caeb4de59024d46641f0d5b18df4a4762785
Nirav Jain <niravjain02@gmail.com>
2026-01-03 22:59:32 -0800

    Initial Commit

---
+++ MVITaskFlow/MVITaskFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVITaskFlowApp.swift
+//  MVITaskFlow
+//
+//  Created by Nirav Jain on 1/3/26.
+//
+
+import SwiftUI
+
+@main
+struct MVITaskFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
