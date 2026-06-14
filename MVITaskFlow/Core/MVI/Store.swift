cf1ca4afa514ea46a67219d3320559927fd757f8
Nirav Jain <niravjain02@gmail.com>
2026-06-14 09:32:29 -0700

    Adjust main actor store dispatch

--- MVITaskFlow/Core/MVI/Store.swift
+++ MVITaskFlow/Core/MVI/Store.swift
@@ -1,4 +1,4 @@
-    //
+//
 //  Store.swift
 //  MVITaskFlow
 //
@@ -10,18 +10,18 @@ public import Combine
 @MainActor
 public final class Store<State, Intent>: ObservableObject {
     @Published public private(set) var state: State
-    private let reducer : Reducer<State,Intent>
-    
+    private let reducer: Reducer<State, Intent>
+
     init(state: State, reducer: @escaping Reducer<State, Intent>) {
         self.state = state
         self.reducer = reducer
     }
-    
+
     public func send(_ intent: Intent) {
         let effects = self.reducer(&self.state, intent)
         run(effects)
     }
-    
+
     private func run(_ effects: [Effect<Intent>]) {
         for effect in effects {
             Task { [weak self] in
